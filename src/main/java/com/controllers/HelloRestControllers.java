fdc98d6cf8e81ea6fa64fc5b0df9f6f48be48783
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 13:46:18 +0530

    [Sachin Shukla]Add uc3 hello using path variable name

--- src/main/java/com/controllers/HelloRestControllers.java
+++ src/main/java/com/controllers/HelloRestControllers.java
@@ -1,6 +1,7 @@
 package com.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,10 @@ public class HelloRestControllers {
     }
     @GetMapping("/q")
     public String hello2(@RequestParam String name) {
+        return "Hello "+ name +" from BridgeLab";
+    }
+    @GetMapping("Hello/{name}")
+    public String hello3(@PathVariable String name) {
         return "Hello "+ name +" from BridgeLab";
     }
 }
